8916ca7364d8f74023f1d0b4243125fa07caee62
Marcio Eduardo <61206362+Coelho-dev99@users.noreply.github.com>
2026-08-18 09:03:18 -0300

    Update 04_counting_regression.py

--- 04_counting_regression.py
+++ 04_counting_regression.py
@@ -1,3 +1,9 @@
+"""
+Contador regressivo
+
+Implementa um contato que vai de um númeiro inicial até 1.
+"""
+
 Contagem = int (input ("Digite o número para a contagem regressiva: ")  )
 
 for i in range (Contagem, 0, -1):
