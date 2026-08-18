fd0ed35659289693133d3c291e29a60e168f4eb8
Marcio Eduardo <61206362+Coelho-dev99@users.noreply.github.com>
2026-08-18 09:02:31 -0300

    Update 03_calculator.py

--- 03_calculator.py
+++ 03_calculator.py
@@ -1,3 +1,9 @@
+"""
+Calculadora Básica
+
+Realizado operações aritméticas básicas: Adição, subtração, multiplicação e divisão.
+"""
+
 Numero1 = float (input (" Digite o primeiro número: ") )
 Numero2 = float (input (" Digite o segundo número: ") )
 
