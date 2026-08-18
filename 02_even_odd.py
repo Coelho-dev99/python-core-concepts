364ad7db26e18ef4e035713151fab9476eebd6da
Marcio Eduardo <61206362+Coelho-dev99@users.noreply.github.com>
2026-08-18 09:01:48 -0300

    Add docstring for even and odd classifier
    Added a docstring to explain the functionality of the script.

--- 02_even_odd.py
+++ 02_even_odd.py
@@ -1,3 +1,8 @@
+"""
+Classificador de números pares e ímpares
+
+Classifica um númeiro inteiro como par ou ímpar usando a lógica de resto da divisão
+"""
 numero = int (input ("Digite um número: ") )
 
 if numero % 2 == 0:
